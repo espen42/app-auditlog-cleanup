3956bf0770d4a0ec102d5d4b302629679d7dacea
Bradnitski <vbr@enonic.com>
2022-08-04 13:34:38 +0200

    context for the query

--- src/main/java/handler/CleanUpHandler.java
+++ src/main/java/handler/CleanUpHandler.java
@@ -1,11 +1,14 @@
 package handler;
 
 import java.time.Instant;
+import java.util.concurrent.Callable;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.enonic.xp.audit.CleanUpAuditLogResult;
+import com.enonic.xp.context.ContextAccessor;
+import com.enonic.xp.context.ContextBuilder;
 import com.enonic.xp.data.ValueFactory;
 import com.enonic.xp.node.FindNodesByQueryResult;
 import com.enonic.xp.node.NodeHit;
@@ -19,12 +22,21 @@ import com.enonic.xp.query.filter.RangeFilter;
 import com.enonic.xp.query.filter.ValueFilter;
 import com.enonic.xp.script.bean.BeanContext;
 import com.enonic.xp.script.bean.ScriptBean;
+import com.enonic.xp.security.IdProviderKey;
+import com.enonic.xp.security.PrincipalKey;
+import com.enonic.xp.security.RoleKeys;
+import com.enonic.xp.security.User;
+import com.enonic.xp.security.auth.AuthenticationInfo;
 
 public class CleanUpHandler
     implements ScriptBean
 {
 
     private static final Logger LOG = LoggerFactory.getLogger( CleanUpHandler.class );
+
+    private static final PrincipalKey NODE_SUPER_USER_KEY = PrincipalKey.ofUser( IdProviderKey.system(), "node-su" );
+
+    private static final User NODE_SUPER_USER = User.create().key( NODE_SUPER_USER_KEY ).login( "node" ).build();
 
     private static final int BATCH_SIZE = 10_000;
 
@@ -34,14 +46,19 @@ public class CleanUpHandler
 
     public CleanUpAuditLogResult run( final String until )
     {
+        return callAsAdmin( () -> doRun( until ) );
+    }
+
+    private CleanUpAuditLogResult doRun( final String until )
+    {
         this.until = Instant.parse( until );
-        LOG.info("Until: " + this.until);
+        LOG.info( "Until: " + this.until );
 
         final CleanUpAuditLogResult.Builder result = CleanUpAuditLogResult.create();
 
         final NodeQuery query = createQuery();
 
-        LOG.info("query: " + query);
+        LOG.info( "query: " + query );
 
         nodeService.refresh( RefreshMode.ALL );
         FindNodesByQueryResult nodesToDelete = nodeService.findByQuery( query );
@@ -49,14 +66,13 @@ public class CleanUpHandler
         long hits = nodesToDelete.getHits();
         final long totalHits = nodesToDelete.getTotalHits();
 
-
         if ( totalHits == 0 )
         {
-            LOG.info( "No auditlog entries to clean.");
+            LOG.info( "No auditlog entries to clean." );
             return CleanUpAuditLogResult.empty();
         }
 
-        LOG.info( String.format( "Cleaning %d auditlog entries...", totalHits));
+        LOG.info( String.format( "Cleaning %d auditlog entries...", totalHits ) );
 
 //        listener.start( BATCH_SIZE );
 
@@ -65,14 +81,18 @@ public class CleanUpHandler
         {
             for ( NodeHit nodeHit : nodesToDelete.getNodeHits() )
             {
-                if (hitCount%100 == 0) {
-                    LOG.info( String.format( "Cleaning auditlog: %d / %d", hitCount, totalHits));
+                if ( hitCount % 100 == 0 )
+                {
+                    LOG.info( String.format( "Cleaning auditlog: %d / %d", hitCount, totalHits ) );
                 }
 
-                try {
+                try
+                {
                     result.deleted( nodeService.deleteById( nodeHit.getNodeId() ).getSize() );
 
-                } catch ( Exception e ) {
+                }
+                catch ( Exception e )
+                {
                     LOG.error( String.format( "Can't remove node - %s", nodeHit.getNodeId() ) );
                 }
 
@@ -88,17 +108,17 @@ public class CleanUpHandler
 
 //        listener.finished();
 
-        LOG.info("Auditlog cleanups is done.");
+        LOG.info( "Auditlog cleanups is done." );
 
         return result.build();
     }
 
     private NodeQuery createQuery()
     {
-        LOG.info("AuditLogConstants time: " + AuditLogConstants.TIME.toString() );
-        LOG.info("NodeIndexPath nodetype: " + NodeIndexPath.NODE_TYPE.toString() );
-        LOG.info("AuditLogConstants nodetype: " + AuditLogConstants.NODE_TYPE.toString() );
-        LOG.info("AuditLogConstants nodetype value: " + ValueFactory.newString( AuditLogConstants.NODE_TYPE.toString() ));
+        LOG.info( "AuditLogConstants time: " + AuditLogConstants.TIME.toString() );
+        LOG.info( "NodeIndexPath nodetype: " + NodeIndexPath.NODE_TYPE.toString() );
+        LOG.info( "AuditLogConstants nodetype: " + AuditLogConstants.NODE_TYPE.toString() );
+        LOG.info( "AuditLogConstants nodetype value: " + ValueFactory.newString( AuditLogConstants.NODE_TYPE.toString() ) );
 
         final NodeQuery.Builder builder = NodeQuery.create()
             .addQueryFilter( ValueFilter.create()
@@ -113,6 +133,16 @@ public class CleanUpHandler
         builder.addOrderBy( FieldOrderExpr.create( AuditLogConstants.TIME, OrderExpr.Direction.ASC ) ).size( BATCH_SIZE );
 
         return builder.build();
+    }
+
+    private <T> T callAsAdmin( final Callable<T> runnable )
+    {
+        return ContextBuilder.from( ContextAccessor.current() )
+            .authInfo( AuthenticationInfo.create().principals( NODE_SUPER_USER_KEY, RoleKeys.ADMIN ).user( NODE_SUPER_USER ).build() )
+            .repositoryId( "system.auditlog" )
+            .branch( "master" )
+            .build()
+            .callWith( runnable );
     }
 
 
